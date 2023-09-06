e308055f7caa6dc00bde1a9fb261ee275f8939cd
Valorieb <valoriebroderick@gmail.com>
2023-09-05 20:20:19 -0400

    adds form validation and success message

--- src/components/CreateAccount.js
+++ src/components/CreateAccount.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
@@ -14,6 +14,8 @@ export const CreateAccount = () => {
     lastName: "",
   };
   const [formValues, setFormValues] = useState(initialValues);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
 
   const handleChange = (e) => {
     // console.log(e.target);
@@ -21,12 +23,52 @@ export const CreateAccount = () => {
     setFormValues({ ...formValues, [name]: value });
     console.log(formValues);
   };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormErrors(validate(formValues));
+    setIsSubmit(true);
+  };
+
+  useEffect(() => {
+    console.log(formErrors);
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      console.log(formValues);
+    }
+  });
+  const validate = (values) => {
+    const errors = {};
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    if (!values.firstName) {
+      errors.firstName = "Please enter your first name";
+    }
+    if (!values.lastName) {
+      errors.lastName = "Please enter your last name";
+    }
+    if (!values.email) {
+      errors.email = "Please enter your email";
+    } else if (!regex.test(values.email)) {
+      errors.email = "email not valid";
+    }
+    if (!values.password) {
+      errors.password = "Please enter your password";
+    } else if (values.password.length < 4) {
+      errors.password = "Password must be at least five characters";
+    } else if (values.password.length > 10) {
+      errors.password = "Password must be less than ten characters";
+    }
+    return errors;
+  };
 
   return (
-    <Card style={{ width: "18rem" }}>
+    <Card style={{ width: "18rem", margin: "auto" }}>
       <Card.Body>
+        {Object.keys(formErrors).length === 0 && isSubmit ? (
+          <div className="uiSuccessMsg">Account Created!</div>
+        ) : (
+          <pre>{JSON.stringify(formValues, undefined, 2)}</pre>
+        )}
         <Card.Title>Create Account</Card.Title>
-        <Form>
+        <Form onSubmit={handleSubmit}>
           <Form.Group className="mb-3" controlId="controlName">
             <div className="field">
               <Form.Label>Enter email</Form.Label>
@@ -39,6 +81,7 @@ export const CreateAccount = () => {
                 onChange={handleChange}
               />
             </div>
+            <p className="errorMsg">{formErrors.email}</p>
           </Form.Group>
           <Form.Group className="mb-3" controlId="controlPassword">
             <div className="field">
@@ -51,6 +94,7 @@ export const CreateAccount = () => {
                 onChange={handleChange}
               />
             </div>
+            <p className="errorMsg">{formErrors.password}</p>
           </Form.Group>
 
           <Row>
@@ -64,6 +108,7 @@ export const CreateAccount = () => {
                   onChange={handleChange}
                 />
               </div>
+              <p className="errorMsg">{formErrors.firstName}</p>
             </Col>
             <Col>
               <Form.Control
@@ -73,12 +118,20 @@ export const CreateAccount = () => {
                 value={formValues.lastName}
                 onChange={handleChange}
               />
+              <p className="errorMsg">{formErrors.lastName}</p>
             </Col>
           </Row>
+          <br />
+          <Button onClick={handleSubmit} variant="primary">
+            Create Account
+          </Button>
         </Form>
-        <br />
-        <Button variant="primary">Create Account</Button>
       </Card.Body>
     </Card>
   );
 };
+
+//NOTES:
+//   <pre> element is displayed and formatted exactly as written.
+//    JSON.stringify converts JavaScript value to JSON string.
+//    This line is similar to using console.log() to test the code.
