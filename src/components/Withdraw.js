73437f9d80fe20a9772d0a52537d2d569f6ce050
Valorieb <valoriebroderick@gmail.com>
2023-09-23 14:46:06 -0400

    adds withdraw form validation

--- src/components/Withdraw.js
+++ src/components/Withdraw.js
@@ -4,15 +4,55 @@ import Card from "react-bootstrap/Card";
 import Form from "react-bootstrap/Form";
 
 const Withdraw = ({ handleWithdrawSubmit, balance }) => {
-  const [withdraw, setWithdraw] = useState(0); // Local withdraw state
+  const [withdraw, setWithdraw] = useState(""); // Local withdraw state
+  const [touched, setTouched] = useState(false);
+  const [formErrors, setFormErrors] = useState({});
 
   const handleChange = (e) => {
     const amount = Number(e.target.value);
-    if (amount > 0) {
-      setWithdraw(amount); // Update local deposit state
+    if (/^\d*\.?\d*$/.test(amount)) {
+      setWithdraw(amount);
+      setFormErrors({});
     } else {
-      setWithdraw(0); // Ensure deposit is non-negative
+      setFormErrors({
+        withdraw: "Withdraw amount must be a valid number",
+      });
     }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const validationError = validateWithdraw(withdraw);
+
+    if (validationError) {
+      setFormErrors({ withdraw: validationError });
+    } else {
+      setFormErrors({});
+      handleWithdrawSubmit(parseFloat(withdraw));
+    }
+  };
+
+  const validateWithdraw = (amount) => {
+    if (isNaN(amount)) {
+      return "Withdrawal amount must be a valid number.";
+    } else if (amount > balance) {
+      return "Withdrawal amount cannot be greater than account balance";
+    }
+    return null;
+  };
+
+  const handleBlur = () => {
+    // Validate on blur if the input has been touched
+    if (touched) {
+      const validationError = validateWithdraw(withdraw);
+      if (validationError) {
+        setFormErrors({ withdraw: validationError });
+      }
+    }
+  };
+
+  const handleFocus = () => {
+    setTouched(true);
   };
 
   return (
@@ -21,21 +61,29 @@ const Withdraw = ({ handleWithdrawSubmit, balance }) => {
         <Card.Title>Withdraw</Card.Title>
         <Card.Text>Balance:</Card.Text>
         <Card.Text id="total">${balance}</Card.Text>
-        <Card.Text>Withdraw Amount:</Card.Text>
-        <Form.Control
-          onChange={handleChange}
-          type="text"
-          placeholder="Enter an amount"
-          value={withdraw}
-        />
-        <br />
-        <Button
-          disabled={withdraw <= 0}
-          onClick={() => handleWithdrawSubmit(withdraw)} // Pass deposit to parent function
-          variant="primary"
-        >
-          Withdraw
-        </Button>
+        <Form onSubmit={handleSubmit}>
+          <Card.Text>Withdraw Amount:</Card.Text>
+          <Form.Control
+            onChange={handleChange}
+            onBlur={handleBlur}
+            onFocus={handleFocus}
+            type="text"
+            placeholder="Enter an amount"
+            value={withdraw}
+            isInvalid={!!formErrors.withdraw}
+          />
+          <Form.Control.Feedback type="invalid">
+            {formErrors.withdraw}
+          </Form.Control.Feedback>
+          <br />
+          <Button
+            disabled={withdraw === ""}
+            onClick={() => handleWithdrawSubmit(withdraw)} // Pass withdraw to parent function
+            variant="primary"
+          >
+            Withdraw
+          </Button>
+        </Form>
       </Card.Body>
     </Card>
   );
